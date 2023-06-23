9706e468d21bc58b7f737c04483f93c4bec25fe6
JavelinSx <zuev499@gmail.com>
2023-06-23 15:54:59 +0300

    go

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,27 +1,29 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
-    'http://localhost:3000/',
-    'http://localhost:3001'
+  'http://localhost:3000/',
+  'http://localhost:3001',
+  'https://spb-obs.ru',
+  'https://api.spb-obs.ru'
 ]
 
 module.exports = (req, res, next) => {
-    const { origin } = req.headers;
-    const { method } = req;
-    const requestHeaders = req.headers['access-control-request-headers'];
-    
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      res.header('Access-Control-Allow-Credentials', true);
-    }
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
-    
-    next();
-    return true;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+  return true;
 };
 
