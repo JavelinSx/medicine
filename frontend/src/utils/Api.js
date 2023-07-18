549d004c8fa48647b3a58b1bc4f0e9f7d74e00b6
JavelinSx <zuev499@gmail.com>
2023-07-18 16:15:11 +0300

    api

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -270,11 +270,11 @@ class Api {
 const url = NODE_ENV === 'production' ? REACT_APP_URL_PROD : REACT_APP_URL_DEV
 
 const MainApi = new Api(
-    url,
+    REACT_APP_URL_PROD,
     {
         "Content-Type": "application/json",
         Accept: "application/json",
-        Origin: url,
+        Origin: REACT_APP_URL_PROD,
     }
 )
 
